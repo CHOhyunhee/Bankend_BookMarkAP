de0c36bff2bfae98dc714a865ef898808da29fdc
chohyunhee <hyunhee425@naver.com>
2025-02-19 17:49:16 +0900

    DTO Projection

--- src/main/java/com/bookmarker/api/controller/BookmarkController.java
+++ src/main/java/com/bookmarker/api/controller/BookmarkController.java
@@ -17,9 +17,18 @@ import java.util.List;
 public class BookmarkController {
     private final BookmarkService bookmarkService;
 
+    //api/bookmarks?page=2
     @GetMapping
-//    public List<Bookmark> getBookmarks(@RequestParam(name = "page", defaultValue = "1") Integer page) {
-      public BookmarksDTO getBookmarks(@RequestParam(name = "page", defaultValue = "1") Integer page) {
-        return bookmarkService.getBookmarks(page);
+    public BookmarksDTO getBookmarks(
+            @RequestParam(name = "page", defaultValue = "1") Integer page,
+            @RequestParam(name = "query", defaultValue = "") String query) {
+        /**
+         * java 11에서 추가된 메소드
+         * isBlank => trim() + isEmpty()
+         */
+        if(query == null || query.isBlank()) {
+            return bookmarkService.getBookmarks(page);
+        }
+        return bookmarkService.searchBookmarks(query, page);
     }
 }
