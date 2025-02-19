816b80b398979957c31e6fa81d82925e432e7fcf
chohyunhee <hyunhee425@naver.com>
2025-02-19 17:29:40 +0900

    flyway apply

--- src/main/java/com/bookmarker/api/service/BookmarkService.java
+++ src/main/java/com/bookmarker/api/service/BookmarkService.java
@@ -1,27 +1,28 @@
 package com.bookmarker.api.service;
 
-import com.bookmarker.api.domain.Bookmark;
 import com.bookmarker.api.domain.BookmarkRepository;
+import com.bookmarker.api.dto.BookmarkDTO;
+import com.bookmarker.api.dto.BookmarksDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class BookmarkService {
     private final BookmarkRepository repository;
-
-    // Constructor Injection, key : shift+insert
-    //  public BookmarkService(BookmarkRepository repository) {
-    //      this.repository = repository;
-    //  }
+    //private final BookmarkMapper mapper;
 
     @Transactional(readOnly = true)
-    public List<Bookmark> getBookmarks() {
-        return repository.findAll();
+    public BookmarksDTO getBookmarks(Integer page) {
+        int pageNo = page < 1 ? 0 : page - 1;
+        Pageable pageable = PageRequest.of(pageNo, 10, Sort.Direction.DESC, "id");
+        Page<BookmarkDTO> bookmarkPage = repository.findBookmarks(pageable);
+        return new BookmarksDTO(bookmarkPage);
     }
-    
 }
