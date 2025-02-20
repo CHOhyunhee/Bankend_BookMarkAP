3d67cc15f4cfa42612351528fb2f7c11ec32fe3c
chohyunhee <hyunhee425@naver.com>
2025-02-20 10:23:38 +0900

    create and search BookMark

--- src/main/java/com/bookmarker/api/domain/BookmarkRepository.java
+++ src/main/java/com/bookmarker/api/domain/BookmarkRepository.java
@@ -1,18 +1,19 @@
 package com.bookmarker.api.domain;
 
 import com.bookmarker.api.dto.BookmarkDTO;
+import com.bookmarker.api.dto.BookmarkVM;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
-    @Query("""
+/*    @Query("""
     select new com.bookmarker.api.dto.BookmarkDTO(b.id, b.title, b.url, b.createdAt) from Bookmark b
+    """)*/
+    @Query("""
+    select new BookmarkDTO(b.id, b.title, b.url, b.createdAt) from Bookmark b
     """)
-//    @Query("""
-//    select BookmarkDTO(b.id, b.title, b.url, b.createdAt) from Bookmark b
-//    """)
     Page<BookmarkDTO> findBookmarks(Pageable pageable);
 
     @Query("""
@@ -22,5 +23,7 @@ public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
     Page<BookmarkDTO> searchBookmarks(String query, Pageable pageable);
 
     Page<BookmarkDTO> findByTitleContainsIgnoreCase(String query, Pageable pageable);
+
+    Page<BookmarkVM> findByTitleContainingIgnoreCase(String query, Pageable pageable);
 
 }
